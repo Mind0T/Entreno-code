50835bd3106d491975abfe6415677288ed588faa
Irvin Soriano <irving.soriano31@gmail.com>
2025-08-07 12:37:20 -0600

    Sigo entrenando

---
+++ C/algoritmia/palindromo/palindromoPro.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+
+//Este es el mero mero de palindromo con apuntadores
+
+int esPalindromo(char *frase);
+
+int main()
+    {
+        // 1 declarar mi variable cadena
+        char cad[100];
+        // 2 pedir una cadena al usuario
+        printf("Ingrese una frase o palabra y le dire si es un palindromo:\n");
+        scanf("%[^'\n']s",cad);
+        // 3 Someto a evaluacion la cadena para ver si es un palindromo
+        if(esPalindromo(cad))// al poner la funcion enviando la cadena evaluara lo que la funcion retorne 0 falso 1 verdadero
+            {
+                printf("La palabra o frase %s\nES UN PALINDROMO\n",cad);
+            }
+        else
+            {
+                printf("La palabra o frase %s\nNO ES UN PALINDROMO\n",cad);
+            }
+    return 0;   
+    }
+
+int esPalindromo(char *frase) // Desarrollo la funcion
+    {
+        //4 Se declaran los punteros y se les asigna la frase que viene desde el mail
+        char *start=frase;
+        char *end=frase;
+        //5 mover le puntero end al final de la cadena
+        while(*end!='\0') //mientras el puntero sea diferente al valor de salida
+            {
+                end++;
+            }
+        end--;
+
+        //6 Comparar los caracteres de inicio y fin
+
+        while(start<end)
+            {
+                //7 Convertir las letras de mayusculas a minusculas
+                if(*start>=65 && *start<= 90)
+                    *start+=32;
+                if(*end>=65 && *end<=90)
+                    *end+=32;
+                
+                //8 Ignorar espacios y caracteres especiales
+                if((*start<97 || *start>122 ) && (*start<48 || *start>57))
+                    {
+                        start++;
+                        continue;
+                    }
+
+                if((*end<97 || *end>122) && (*end<48 || *end>57))
+                    {
+                        end--;
+                        continue;
+                    }
+                //9 Compara los caracteres
+                if(*start!=*end)
+                    {
+                        return 0;
+                    }
+                start++;
+                end--;
+            }
+        return 1;
+    }
