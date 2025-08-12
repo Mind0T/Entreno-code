62cfc324260c6bddb57320178be3301f3821980c
Irvin Soriano <irving.soriano31@gmail.com>
2025-08-12 16:10:40 -0600

    Entreno condicionales python

---
+++ python/logicaPython/condicionales.py
@@ -0,0 +1,23 @@
+#Logico Boolean Booleano
+#verdadero/ falso: true/false
+
+""" dato=5==3
+
+print(dato)
+print(type(dato)) """
+
+""" numero=int(input("Ingrese un numero: "))
+
+if numero==10:
+    print("El numero es 10")
+else:
+    print("El numero es diferente a 10")
+
+print("\nTerminando programa") """
+
+print("Ingresa tu nombre")
+nombre= input()
+if nombre=="Irving":
+    print("Bienvenido ",nombre)
+else:
+    print("Ke nombre tan raro")
---
+++ python/logicaPython/repaso.py
@@ -0,0 +1,6 @@
+print("Bienvenido al conversor de millas a kilometros\nIngrese una cantidad en millas y las convertire en kilometros")
+
+millas=float(input("Millas: "))
+
+kilometros=millas*1.609
+print("Tu ingresasteL",millas,"millas\nY convertido es:",kilometros,"kilometros")
