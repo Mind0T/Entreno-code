b896c85c1ad3c1fb6da72b5ca31414e39ead77e4
Irving Jair Soriano Rosales <168896232+Mind0T@users.noreply.github.com>
2025-08-15 11:42:12 -0600

    repaso del viernes

--- C/algoritmia/repaso.c
+++ C/algoritmia/repaso.c
@@ -4,53 +4,54 @@ int esPalindromo(char *ptr);
 
 int main()
     {
-        char cad[300];
-        puts("Hola Ingrese de favor una palabra o frase y le dire si es un palindromo o no");
+        char cad[200];
+        printf("Que tal ingresa una palabra o frasse y te dire si es un palindromo\n");
         scanf("%[^'\n']s",cad);
         if(esPalindromo(cad))
             {
-                printf("La palabra o frase:%s\nES UN PALINDROMO\n",cad);
+                printf("La palabra o frase %s\nBIEN ES UN PALINDROMO\n",cad);
             }
         else
             {
-                printf("La palabra o frase:%s\nNO ES UN PALINDROMO\n",cad);
+                printf("Oye la palabra o frase %s\nNO ES UN PALINDROMO\n",cad);
             }
         return 0;
     }
 
-int esPalindromo(char *ptr)
-    {
-        char *end=ptr;
-        char *start=ptr;
+    int esPalindromo(char *ptr)
+        {
+            char *end;
+            char *start;
+            while(*end!='\0')
+                {
+                    end++;
+                }
+            end--;
+            while(start<end)
+                {
+                    if(*start>=65 && *start<=90)
+                        *start+=32;
+                    
+                    if(*end>=65 && *start<=90)
+                        *end+=32;
 
-        while(*end!='\0')
-            {
-                end++;
-            }
-        end--;
-
-        while(start<end)
-            {
-                if(*start>=65 && *start<=90)
-                    *start+=32;
-                if(*end>=65 && *end<=90)
-                    *end+=32;
-                if((*start<48 || *start>57 ) && (*start<97 || *start>122 ))
-                    {
-                        start++;
-                        continue;
-                    }
-                if((*end<48 || *end>57) && (*end<97 || *end>122))
-                    {
-                        end--;
-                        continue;
-                    }
-                if(*start!=*end)
-                    {
-                        return 0;
-                    }
-                end--;
-                start++;
-            }
-        return 1;
-    }
+                    if((*start<48 && *start>57) && (*start<97 && *start>122))
+                        {
+                            start++;
+                            continue;
+                        }
+                        
+                    if((*end<48 || *end>57) && (*end<97 && *end>122))
+                        {
+                            end--;
+                            continue;
+                        }
+                    if(*start!=*end)
+                        {
+                            return 0;
+                        }
+                    end--;
+                    start++;
+                }
+            return 1;
+        }
