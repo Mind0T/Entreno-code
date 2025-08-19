6ff11897b4786f2222ccd1e9ebe0f0690456c62b
Mind0T <irving.soriano31@gmail.com>
2025-08-19 15:37:13 -0600

    Entreno python tuplas 19 Ago

--- python/logicaPython/tienda.py
+++ python/logicaPython/tienda.py
@@ -1,112 +1,126 @@
-from datetime import datetime # libreria para fechas
+#18 importar la libreria para mi variable tiempo
+from datetime import datetime
 
-print("*****************************")
-print("********BIENVENIDO A*********")
-print("***A LA TIEDA DE MASCOTAS****")
-print("*****************************")
+print("**********************")
+print("******Bienvenido********")
+print("***a la tienda de******")
+print("********mascotas******")
 
 
-inventario={ ##este afuerza si tiene que ir aca
-        "perro":10,
-        "gato":8,
-        "pajaros":25,
-        "iguana":2
-    }
-
-""" num_perros=10
-num_gatos=8
-num_pajaros=25
- """
-
-animalesTotales=0
-
+#8 Defino la variable de tipo Global diccionario clave valor
+# para la funcion de mostrar inventario
+inventario={
+    "perro":10,
+    "gato":8,
+    "pajaro":25,
+    "iguana":2,
+    "conejo":1
+}
+totalAnimales=0
+#9 Defino la variable de totalAnimales y la itero para obtener el total de animales
 for val in inventario.values():
-    animalesTotales+=val
+    totalAnimales+=val
 
-print("Cual es Nombre")
-nombre=input()
-print("Cual es su Apellido: ")
-apellido=input()
-print("\nGracias por visitarnos:",nombre,"",apellido,"\n")
-
+#11 Defino la variable compra que ocupare en hacer compras y mostrare en mostrar compras
 compras=[]
 
+#1 genero variables de nombre y apellido y mando a saludar
+nombre=input("Cual es su nombre\n")
+apellido=input("Cual es su apellido\n")
+print(f"\nBienvenido {nombre} {apellido}\n")
 
-def mostraMenu():
-    print("Selecciona la opcion que desea")
-    print("1- Conocer cuantos animales tiene la tienda")
-    print("2- Comprar un animal")
-    print("3- Mostrar compras")
-    print("4. Salir del programa")
+
+#2 Creo una funcion con el menu
+
+
+def mostrarMenu():
+    print("Escoge una opcion")
+    print("1- Mostrar el inventario")
+    print("2 Compra un animal")
+    print("3 Mostrar las compras")
+    print("4 Salir de la tienda")
+#3 con base al menu declaro las funciones
 
 def mostrarInventario():
-
-    print("\n***INVENTARIO***\n")
-    for key, value in inventario.items():
-        print(f"  {key}: {value}")
-    print("\nEn total tenemos: ",animalesTotales,"animales\n")
-
-    """   print("\nActualmente contamos con:")
-    print("Perros:",num_perros)
-    print("Gatos:",num_gatos)
-    print("Pajaros: ",num_pajaros)
-    print("En total tenemos: ",num_perros+num_gatos+num_pajaros,"animales") """
-
-
-
-def mostrarCompras():
-    print("")
-    print("Compras Realizadas")
-
-    for compra in compras:
-        print(f"   {compra[0]} compro {compra[1]} el {compra[2]}")
-
+    #7 Desarrollo la funcion de mostrar inventario
+    print("\n")
+    print("Actualmente contamos con:\n")
+    for key, val in inventario.items():
+        print(f"{key}: {val}")
+    print(f"\nEn total son {totalAnimales} animales")
+    print("\n")
 
 def comprarAnimal():
+    #10 Desarrollo la funcion de hacer comprar
+    print("\n")
+    #12 Defino la variable carrito que solo vivira aqui
     carrito=[]
-    while True:
-
-        print("\n-Escribe f para terminar la lista")
-        print("-Escribe v para ver tu carrito")
-        animal=input("-Ingresa el animal que deseas comprar,(Solo puedes agregar uno de cada especie)\nOPCION: ")
-        if animal=="f":
-            break
-        if animal=="v":
-            print(f"Tu carrito de compras contien {carrito}")
-            continue
-        if animal not in inventario:
-            print(f"Lo sentimos no contamos con el animal {animal}")
-        elif inventario[animal]==0:
-            print(f"Lo sentimos, no tenemos en existencia el animal: {animal}")
-        elif animal not in carrito:
-            carrito.append(animal)
-        else:
-            print("\nLo sieto ese animal ya esta en tu carrito")
-
-    print("\nEl contenido de tu carrito es:")
-    for animal in carrito:
-        print("  ",animal)
-        inventario[animal]-=1
     
-    fecha=datetime.now() #asi invoca la fecha
-    compras.append((nombre,carrito,fecha))
-    #Compras tiene longitud 1 (solo tengo 1 compra)
-    # Es una tupla, Nombre, Carrito, Fecha        
+    #14 Hago un while para controlar las opciones
+    while True:
+        #13 Doy indicacions
+        print("Que bueno que te animes a comprar un animalito")
+        print("Elige una opcion:\n")
+        print("-Escribe f para terminar tus compras")
+        print("-Escribe v para ver tu carrito")
+        print("-Elige un animal\n")
+        comprasOpcion=input("Opcion: ")
 
+        if comprasOpcion=="f":
+            print("Finalizando compra\n")
+            break
+        if comprasOpcion=="v":
+            print("\nLlevas esto en tu carrito:")
+            print(f"{carrito}\n")
+            continue
+        #15 aqui va la parte dificil de los diccionario
+        if comprasOpcion not in inventario:
+            print(f"\nLo sentimos no vendemos {comprasOpcion}\nPrueba con otro\n")
+        elif inventario[comprasOpcion]==0:
+            print(f"\nLo siento ya no tengo inventario de {comprasOpcion}\nPrueba con otro\n")
+        elif comprasOpcion not in carrito:
+            carrito.append(comprasOpcion)
+            print(f"\nSe ha agregado {comprasOpcion} a tu carrito\n")
+        else:
+            print(f"\nYa tienes agregado {comprasOpcion} en tu carrito\nPrueba con otro\n")
+        
+    #16 Indicar que haz finalizado compras y mostrar lita
+    print(f"\nCompra finalizada compraste:\n")
+    for comprasOpcion in carrito:
+        print(f"  ",comprasOpcion)
+        inventario[comprasOpcion]-=1
+    #17 Definir variable fecha e importar la variable compras
+    fecha=datetime.now()
+    compras.append((nombre,carrito,fecha))
+
+    print("\n")
+
+def mostrarCompras():
+    print("\n")
+    for compra in compras:
+        print(f"   {compra[0]} compro {compra[1]} el {compra[2]}")
+    print("\n")
+
+#4 Construye el while para que mande a las funcione
 
 while True:
-    mostraMenu()
-    respuesta=int(input("Respuesta: "))
+    mostrarMenu() #5 Muestro el menu
+    opcion=int(input("\nOpcion: \n"))
 
-    if respuesta== 1:
+    #6 Evaluo la opcion del usuario y doy las likeo a las funciones
+    if opcion==1: 
         mostrarInventario()
-
-    elif respuesta==2:
+    elif opcion==2:
         comprarAnimal()
-    
-    elif respuesta==3:
+    elif opcion==3:
         mostrarCompras()
-
-    elif respuesta==4:
-        print("\nHasta luego")
+    elif opcion==4:
+        print("\n")
+        print("Hasta luego, regrese pronto")
         break
+    else:
+        print("\n")
+        print("Ingrese una opcion valida")
+        print("\n")
+        continue
+        
