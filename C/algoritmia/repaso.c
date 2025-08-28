cc7da470d5f6a48b709fc67dc1b0b243d244dd32
Irvin Soriano <irving.soriano31@gmail.com>
2025-08-28 13:04:36 -0600

    Entreno C 28 Ago

--- C/algoritmia/repaso.c
+++ C/algoritmia/repaso.c
@@ -4,23 +4,25 @@ int esPalindromo(char *frase);
 
 int main()
     {
-        char cadena[200];
-        printf("Hola 22 Ago Ingresa una palabra o frase y te dire si es un palindromo\n");
-        scanf("%[^'\n']s",cadena);
-        if (esPalindromo(cadena))
+        printf("Hola 28 Ago\nIngresa una palabra o frase y te dire si es un Palindromo\n");
+        char cad[300];
+        scanf("%[^'\n']s",cad);
+        if(esPalindromo(cad))
             {
-                printf("La palabra o frase %s\nES UN PALINDROMO\n",cadena);
+                printf("La palabra o frase %s\nES UN PALINDROMO\n",cad);
             }
-        else
+        else    
             {
-                printf("La palabra o frase %s\nNO ES UN PALINDROMO\n",cadena);
+                printf("La palabra o frase %s\nNO ES UN PALINDROMO\n",cad);
             }
         return 0;
     }
 
 int esPalindromo(char *frase)
     {
-        char *end=frase, *start=frase;
+        char *end=frase;
+        char *start=frase;
+
         while(*end!='\0')
             {
                 end++;
@@ -32,20 +34,19 @@ int esPalindromo(char *frase)
                     *start+=32;
                 if(*end>=65 && *end<=90)
                     *end+=32;
-
                 if((*start<48 || *start>57) && (*start<97 || *start>122))
                     {
                         start++;
                         continue;
                     }
-                if((*end<48 || *end>57) && (*end<97 || *end>122))
+                if((*end <48 || *end>57) && (*end<97 || *end>122))
                     {
                         end--;
                         continue;
-                    }
-                if(*end!=*start)
+                    } 
+                if(*start!=*end)
                     return 0;
-                
+
                 end--;
                 start++;
             }
