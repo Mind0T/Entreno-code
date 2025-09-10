b88ddbb2b423565cb3835598c21120d2603fee88
Irvin Soriano <irving.soriano31@gmail.com>
2025-09-09 23:46:51 -0600

    hasta mi primer modelo

---
+++ python/inteligenciaArtificialDom/notas.py
@@ -0,0 +1,59 @@
+# para checar si tengo instalda 
+"""
+pip list | grep scikit-learn
+"""
+
+#Tipos de algoritmos de ML
+
+"""
+Aprendizaje supervisado
+    Conjunto de datos con caracteristicas y la etiqueta correcta
+        Utilizados para:
+            -Clasificacion
+            -Regresion
+
+Aprendizaje no supervisado
+    Contamos con datos sin etiquetas. El modelo busca patrones y relaciones
+        Utilizados para:
+            -Clusterizacion
+            -
+
+Aprendizaje por refuerzo
+    Un agente aprende en un ambiente por medio de acciones y retroalimentacion
+        Utilizados para:
+            -Robot, cambia con el ambiente y a tarea
+"""
+
+#Datos
+"""
+Base de cualquier proyecto
+Basura entra basura sale
+Contar con buenos datos no es simple
+    Ciclo de vida de los datos
+        Obtencion
+        Limpieza
+        Caracteristicas
+    Entrenamiento 80%
+    Prueba 20%
+"""
+#Pandas
+"""
+Leer, organizar y analizar conjunto de datos de manera sencilla y rapida
+"""
+#Numpy
+"""
+Trabajar matrices y arreglos de multiples dimensiones y hacer operaciones con ellos de alto desempeno
+optimizando el hardware
+"""
+#Seaborn y Matplolib
+"""
+Visualizacion de datos
+"""
+#Scikit learn
+"""
+Modelos estadisticos, clasificacion regresion
+"""
+#TensorFlow Pytorch 
+"""
+Entrenar modelos de aprendizaje profundo
+"""
